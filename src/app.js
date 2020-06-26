6e60500bea802a2c5dcc34b442a407833e608685
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-06-26 15:48:25 -0300

    PinG-PonG-JS-Vanilla

---
+++ src/app.js
@@ -0,0 +1,28 @@
+var folhaDesenho = document.getElementById("folha");
+var areaDesenho = folhaDesenho.getContext("2d");
+
+var larguraCampo = 600;
+var alturaCampo = 500;
+var larguraLinha = 10;
+var larguraBola = 10;
+var alturaRaquete = 100;
+        
+areaDesenho.fillStyle = '#286047';
+        
+//Mesa
+areaDesenho.fillRect(0, 0, larguraCampo, alturaCampo);
+
+areaDesenho.fillStyle = '#ffffff';
+        
+//linha central
+areaDesenho.fillRect(larguraCampo/2 - larguraLinha/2, 0, larguraLinha, alturaCampo);
+        
+//Raquete 1
+areaDesenho.fillRect(0, 30, larguraLinha, alturaRaquete);
+        
+//Raquete 2
+areaDesenho.fillRect(larguraCampo - larguraLinha, 330, larguraLinha, alturaRaquete);
+
+//Bola "quadrada"
+areaDesenho.fillRect(30, 30, larguraBola, larguraBola);
+
